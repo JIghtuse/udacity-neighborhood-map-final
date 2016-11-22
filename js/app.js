2ea130c8815e17ffeb9fd31e8210b5781009be56
Boris Egorov <egorov@linux.com>
2016-11-23 01:09:17 +0700

    index, js: Preload Google maps, add map/markers in ViewModel

--- js/app.js
+++ js/app.js
@@ -1,27 +1,34 @@
-function addMarker(location, map, label) {
+function addMarker(pos, map) {
     var marker = new google.maps.Marker({
-        position: location,
+        position: pos,
         map: map
     });
 }
 
-function initMap() {
-    var nsk = {lat: 55.022, lng: 82.911983};
+function Location(name, position) {
+    this.name = name;
+    this.position = position;
+}
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: nsk
-    });
+function ViewModel() {
+    var self = this;
 
-    var locations = [
-        {lat: 55.019988, lng: 82.888634},
-        {lat: 55.009065, lng: 82.953222},
-        {lat: 55.027040, lng: 82.920691},
-        {lat: 55.055372, lng: 82.879887},
-        {lat: 54.986975, lng: 82.874103}
+    self.locations = [
+        new Location("Aquapark", {lat: 55.019988, lng: 82.888634}),
+        new Location("Galileo", {lat: 55.009065, lng: 82.953222}),
+        new Location("Pliny the Elder", {lat: 55.027040, lng: 82.920691}),
+        new Location("Novosibirsk Zoo", {lat: 55.055385, lng: 82.879814}),
+        new Location("Monument of Glory", {lat: 54.986975, lng: 82.874103}),
     ];
 
-    locations.forEach(function(location) {
-        addMarker(location, map);
+    self.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {lat: 55.022, lng: 82.912}
+    });
+
+    self.locations.forEach(function(location) {
+        addMarker(location.position, self.map);
     });
 }
+
+ko.applyBindings(new ViewModel());
